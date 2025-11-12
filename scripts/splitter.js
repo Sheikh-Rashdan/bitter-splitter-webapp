924d9add5629301814dccf8106cc9b784d2a7a87
Sheikh-Rashdan <sheikhmohamedrashdan@gmail.com>
2025-11-12 19:50:50 +0530

    create-group page

--- scripts/splitter.js
+++ scripts/splitter.js
@@ -12,5 +12,11 @@ function generateGroupHTML() {
     groupCardContainerElement.innerHTML = generatedHTML;
 }
 
+const createGroupButtonElement = document.querySelector('.js-create-group-button');
 const groupCardContainerElement = document.querySelector('.js-group-card-container');
+
 generateGroupHTML();
+
+createGroupButtonElement.addEventListener('click', () => {
+    location.href = 'pages/create-group.html'
+});
